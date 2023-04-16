ffcb0cdefeb55d8296c035784d2f75443130dbcb
Mariuskvaal <mariuskvaal1@gmail.com>
2023-04-16 14:40:49 +0200

    updating the registration form

---
+++ registrer.mjs
@@ -0,0 +1,16 @@
+const form = document.querySelector("#registrerForm");
+
+form.addEventListener("submit", (event) => {
+
+    const form = event.target;
+
+    const name = form.name.value
+    const email = form.email.value
+    const password = form.password.value
+    const banner = form.banner.value
+    const avatar = form.avatar.value
+
+
+     
+
+})
