cb93e0431ff76aa077703917f5cce6d4fb7d3568
Mariuskvaal <mariuskvaal1@gmail.com>
2023-05-19 23:30:40 +0200

    updatin

--- Posts/script.js
+++ Posts/script.js
@@ -1,11 +1,44 @@
+import { load } from "/storage.mjs";
+
+const token = load("token");
+
+fetch('https://api.noroff.dev/api/v1/social/posts', {
+  method:'get',
+  headers: {
+    "Content-Type": "application/json",
+    "Authorization": `Bearer ${token}`
+  }
+})
+
+.then(response => response.json())
+.then(json => console.log(json))
+
+const createHtml = document.querySelector("#container");
+
+createHtml.createElement('div')
+
+
+
+
+/*
+const getData = () => {
+  fetch('https://api.noroff.dev/api/v1/social/posts').then(Response => {
+    console.log(response);
+  })
+}
+
+
+
+
+
 //console.log("hello world");
 
 function fetchImagefromAPI() {
     const apiKey = 'YOUR_API_KEY';
-    fetch('https://any-anime.p.rapidapi.com/anime/img', {
+    fetch('https://api.noroff.dev/api/v1/posts', {
       method: 'GET',
       headers: {
        
       }
     })
-   }
+   }*/
